b5769c8de85b196a980a76bcdfca6af59c8bdddf
Hyeon-Uk <rlagusdnr120@gmail.com>
2022-06-21 19:04:23 +0900

    user controller created with join

---
+++ src/main/java/com/example/commerce/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.example.commerce.controller;
+
+import com.example.commerce.dto.req.JoinFormDto;
+import com.example.commerce.entity.User;
+import com.example.commerce.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@Slf4j
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/join")
+    public String joinForm(){
+        return "JoinForm";
+    }
+
+    @PostMapping("/join/process")
+    public String joinProcess(JoinFormDto formDto){
+        log.info("user email={},name={},password={}",formDto.getEmail(),formDto.getName(),formDto.getPassword());
+        userService.join(formDto);
+        return "redirect:/";
+    }
+}
